254c0e6756a22fc6f67cae05b7b047ae2ce42a3e
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2022-07-05 18:23:38 -0400

    ws copy paste

--- www/ws.py
+++ www/ws.py
@@ -1,7 +1,23 @@
 
+# Builting
+import traceback
+
+# 3rdparty
 import aiohttp.web
 
+async def maybe_await(fun, on_exception=None):
+  try:
+    return await fun()
+  except:
+    traceback.print_exc()
+    if on_exception != None:
+      return on_exception()
+    return None
+
+all_ws = []
+
 async def handle_ws(req):
+  global all_ws
   peername = req.transport.get_extra_info('peername')
   host = 'unk'
   if peername is not None:
@@ -11,6 +27,28 @@ async def handle_ws(req):
 
   ws = aiohttp.web.WebSocketResponse()
   await ws.prepare(req)
+
+  all_ws.append(ws)
+
+  async for msg in ws:
+    try:
+      if msg.type == aiohttp.WSMsgType.TEXT:
+        print('WS From {}: {}'.format(host, msg.data))
+        
+        if msg.data.startswith('message='):
+          continue
+
+        # Broadcast to everyone else
+        # with CodeTimer('Broadcast to everyone else', unit='ms'):
+        #   await asyncio.gather(*[ maybe_await(lambda: w.send_str(msg.data)) for w in all_ws if w != ws])
+        await asyncio.gather(*[ maybe_await(lambda: w.send_str(msg.data)) for w in all_ws if w != ws])
+        
+      elif msg.type == aiohttp.WSMsgType.ERROR:
+        print('ws connection closed with exception {}'.format(ws.exception()))
+    except:
+      traceback.print_exc()
+
+  all_ws.remove(ws)
 
   return ws
 
